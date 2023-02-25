9b5d348a35723c9a516fd86af0571b926777a73c
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-02-25 08:50:32 +0500

    updated

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,4 +1,6 @@
 from .models import Category
+
+
 def categories(request):
     cats = Category.objects.all().order_by("-id")
     # accounts = Account.objects.all()
@@ -9,8 +11,17 @@ def categories(request):
     #     phone = Account.objects.filter(name=Account.PHONE_NUMBER).first().link
     # if Account.objects.filter(name=Account.INSTAGRAM).exists():
     #     instagram = Account.objects.filter(name=Account.INSTAGRAM).first().link
-    
+    facebook = "https://m.facebook.com/story.php?story_fbid=pfbid02nk2mYyBH9hzgUfUTWoZGmeWnFmCnPsKPcbQtbbwNZd6mUdFPaf1RxurD5PpkX613l&id=100063572289527&mibextid=qC1gEa"
+    instagram = "https://www.instagram.com/zvukon.uz/"
+    telegram = "https://t.me/zvukonuzb"
+    phone = "+998909007666"
+    adress = "Малика Базар А-блок 7магазин"
     context = {
-        "cats":cats,
+        "cats": cats,
+        "facebook": facebook,
+        "instagram": instagram,
+        "telegram": telegram,
+        "phone" : phone,
+        "adress": adress,
     }
     return context
